96e70fe24668b643c8bba4ff8a466a1e746ca4be
João Pinheiro <joao.castropinheiro@gmail.com>
2021-05-12 16:07:30 +0100

    tweaks

--- spigot/backup.sh
+++ spigot/backup.sh
@@ -1,1 +1,2 @@
 docker run --volumes-from spigot -v $(pwd):/backup ubuntu:20.04 tar cf /backup/spigot-$(date -Iminutes).tar /spigot
+sudo chown $(id -un): spigot-$(date -Iminutes).tar
--- spotifyd/spotifyd.sh
+++ spotifyd/spotifyd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-amixer sset 'Master',0 unmute
+amixer sset Master,0 unmute
+amixer sset Master,0 50%
 exec /spotifyd/spotifyd --no-daemon -c /spotifyd/cache -d pinecone "$@"

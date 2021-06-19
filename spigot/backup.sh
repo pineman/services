d66e42a8dad08670f56fb49dcd760cf014aebd8d
João Pinheiro <joao.castropinheiro@gmail.com>
2021-06-19 19:05:23 +0100

    timestamp for backup

--- spigot/backup.sh
+++ spigot/backup.sh
@@ -1,2 +1,2 @@
-docker run --volumes-from spigot -v $(pwd):/backup ubuntu:20.04 tar cf /backup/spigot-$(date -Iminutes).tar /spigot
-sudo chown $(id -un): spigot-$(date -Iminutes).tar
+docker run --volumes-from spigot -v $(pwd):/backup ubuntu:20.04 tar cf /backup/spigot-$(date +%F_%H.%M).tar /spigot
+sudo chown $(id -un): *.tar

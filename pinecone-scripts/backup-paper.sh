59ef007862543ba5f49405b02cb77610755a0f37
João Pinheiro <joao.castropinheiro@gmail.com>
2021-07-18 22:56:38 +0100

    add backup scripts from dotfiles

--- pinecone-scripts/backup-paper.sh
+++ pinecone-scripts/backup-paper.sh
@@ -1,2 +1,5 @@
+docker exec paper /usr/bin/tmux send-keys save-off C-m save-all C-m
+sleep 10s
 docker run --volumes-from paper -v $(pwd):/backup ubuntu:20.04 tar cf /backup/paper-$(date +%F_%H.%M).tar /paper
 sudo chown $(id -un): *.tar
+docker exec paper /usr/bin/tmux send-keys save-on C-m

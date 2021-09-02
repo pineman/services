7187b145cc5b0729b9ca6942b53a60d2344fd166
João Pinheiro <joao.castropinheiro@gmail.com>
2021-09-02 12:15:26 +0100

    small paper tweaks

--- pinecone-scripts/backup-paper.sh
+++ pinecone-scripts/backup-paper.sh
@@ -1,5 +1,6 @@
 docker exec paper /usr/bin/tmux send-keys save-off C-m save-all C-m
 sleep 10s
-docker run --volumes-from paper -v $(pwd):/backup ubuntu:20.04 tar cf /backup/paper-$(date +%F_%H.%M).tar /paper
-sudo chown $(id -un): *.tar
+FILENAME=paper-$(date +%F_%H.%M)
+sudo tar cf /home/pineman/$FILENAME.tar /home/pineman/paper
+sudo chown $(id -un): /home/pineman/$FILENAME.tar
 docker exec paper /usr/bin/tmux send-keys save-on C-m

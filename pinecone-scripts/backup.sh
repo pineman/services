cf60e8e098fcb36b16fb2f92b0ce19fc34b4585d
João Pinheiro <joao.castropinheiro@gmail.com>
2021-09-22 20:09:58 +0100

    traefik ingress

--- pinecone-scripts/backup.sh
+++ pinecone-scripts/backup.sh
@@ -6,10 +6,10 @@ set -euxo pipefail
 \fd -I -H --glob '._*' /home/pineman/Documents -x rm
 last_recv=$(zfs list -H -o name -t snapshot -s creation hdd | tail -n 1 | cut -d'@' -f2)
 new=$(date +%s | md5sum | cut -c1-8)
-docker exec paper /usr/bin/tmux send-keys save-off C-m save-all C-m
+docker exec paper /usr/bin/tmux send-keys save-off C-m save-all C-m || true
 sleep 10s
 zfs snapshot -r ssd@$new
-docker exec paper /usr/bin/tmux send-keys save-on C-m
+docker exec paper /usr/bin/tmux send-keys save-on C-m || true
 zfs send -R -I $last_recv ssd@$new | zfs recv -Fdu hdd
 b2-linux sync --noProgress --keepDays 30 --threads 8 --replaceNewer /home/pineman/Documents/.zfs/snapshot/$new b2://pineman-Documents
 zpool status; zpool list

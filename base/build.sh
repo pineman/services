120a5b239b23be0ab553557ff01e93c35e10d58e
João Pinheiro <joao.castropinheiro@gmail.com>
2021-09-23 16:37:06 +0100

    improve base with locales etc.

--- base/build.sh
+++ base/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-VERSION=1.0.0
-docker build -t registry.gitlab.com/pineman/services/base:$VERSION .
+VERSION=1.3.0
+docker build --no-cache -t registry.gitlab.com/pineman/services/base:$VERSION .
+docker tag registry.gitlab.com/pineman/services/base:$VERSION registry.gitlab.com/pineman/services/base:latest
 docker push -a registry.gitlab.com/pineman/services/base

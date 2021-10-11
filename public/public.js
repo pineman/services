18f8a58aebfa6bc86df7d755aa3908c09b10e4c6
João Pinheiro <joao.castropinheiro@gmail.com>
2021-10-11 13:31:05 +0100

    public logs

--- public/public.js
+++ public/public.js
@@ -7,17 +7,29 @@ process.chdir('/public');
 http.createServer((req, res) => {
 	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	let dir = path.basename(decodeURI(req.url));
-	console.log(`[${ip}] ${dir}: request`);
+	const log = (msg) => {
+		console.log(`[${ip} ${dir}] ${msg}`);
+	};
+	log('request');
 	let stat = fs.statSync(dir, {throwIfNoEntry: false})
 	if (stat === undefined || !stat.isDirectory()) {
-		console.log(`[${ip}] ${dir}: not a dir`);
+		log('not a dir');
 		res.writeHead(404);
 		res.end();
 		return;
 	}
 	let zip = child_process.spawn('zip', ['-0', '-r', '-', dir]);
+	zip.on('error', (err) => {
+		log(`failed to start zip: ${err}`);
+	});
+	zip.stderr.on('data', (stderr) => {
+		log(`stderr: ${stderr}`);
+	});
 	res.writeHead(200, {'Content-Type': 'application/zip'});
+	log('piping zip');
 	zip.stdout.pipe(res);
-	console.log(`[${ip}] ${dir}: success`);
+	zip.on('close', (code) => {
+		log(`exit: ${code} ${code == 0 ? 'success' : ''}`);
+	});
 }).listen(8035);
 

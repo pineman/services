c0aeec2c38f41f9f0a8e9b5b2ffd1f3028fcba96
João Pinheiro <joao.castropinheiro@gmail.com>
2022-01-18 12:21:38 +0000

    updates

--- base/build.sh
+++ base/build.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 VERSION=1.3.0
 docker build --no-cache -t registry.gitlab.com/pineman/services/base:$VERSION .
 docker tag registry.gitlab.com/pineman/services/base:$VERSION registry.gitlab.com/pineman/services/base:latest
-docker push -a registry.gitlab.com/pineman/services/base
+docker push registry.gitlab.com/pineman/services/base:$VERSION

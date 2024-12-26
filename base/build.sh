22e293f026fb5e56e87fb8c351bce28d896ea2be
João Pinheiro <joao.castropinheiro@gmail.com>
2024-12-26 18:23:46 +0000

    use github

--- base/build.sh
+++ base/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 VERSION=1.3.2
-docker build --no-cache -t registry.gitlab.com/pineman/services/base:$VERSION .
-docker tag registry.gitlab.com/pineman/services/base:$VERSION registry.gitlab.com/pineman/services/base:latest
-docker push registry.gitlab.com/pineman/services/base:$VERSION
-docker push registry.gitlab.com/pineman/services/base:latest
+docker build --no-cache -t ghcr.io/pineman/services/base:$VERSION .
+docker tag ghcr.io/pineman/services/base:$VERSION ghcr.io/pineman/services/base:latest
+docker push ghcr.io/pineman/services/base:$VERSION
+docker push ghcr.io/pineman/services/base:latest

d653861e9dd05c56053cc4b33dcee7ff4007c61a
João Pinheiro <joao.castropinheiro@gmail.com>
2025-08-10 18:28:47 +0100

    nit

--- new_hetzner_docker_host.sh
+++ new_hetzner_docker_host.sh
@@ -5,8 +5,9 @@ NAME=services
 IP=$(hcloud server list | grep $NAME | awk '{ print $4 }' || true)
 if [[ -z $IP ]]; then
   hcloud server create --name $NAME --type cx22 --image docker-ce --ssh-key pineman --location nbg1
+  IP=$(hcloud server list | grep $NAME | awk '{ print $4 }' || true)
 fi
-IP=$(hcloud server list | grep $NAME | awk '{ print $4 }' || true)
+
 while :; do
   ssh -T -o StrictHostKeyChecking=accept-new root@"$IP" exit 0 && break &>/dev/null
   sleep 5
